26ec31aa8e2c3774700ab6bd800f089156d0bf8d
Krastanov's agent <github.agent@krastanov.org>
2026-09-12 04:29:46 +0000

    Fix CI runners and JET analysis

--- test/test_jet.jl
+++ test/test_jet.jl
@@ -4,12 +4,6 @@ using JET
 using Test
 using VNGraphs
 
-rep = report_package("VNGraphs";
-    ignored_modules=(
-        LastFrameModule(Base),
-    )
-)
-@show rep
-@test length(JET.get_reports(rep)) == 0
+JET.test_package(VNGraphs; target_modules=(VNGraphs, VNGraphs.Lib))
 
 end
